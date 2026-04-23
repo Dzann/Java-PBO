e85bf39d0916f760e775965522318dde18a76fa8
Dzann <gidzanmuh@gmail.com>
2026-04-23 21:13:23 +0700

    Tugas 6

---
+++ src/Sesi_6/Tugas/Main.java
@@ -0,0 +1,25 @@
+package Sesi_6.Tugas;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test Student
+        Student s = new Student("Budi", "Jakarta");
+        s.addCourseGrade("Math", 80);
+        s.addCourseGrade("English", 90);
+
+        System.out.println(s);
+        s.printGrades();
+        System.out.println("Average: " + s.getAverageGrade());
+
+        System.out.println();
+
+        // Test Teacher
+        Teacher t = new Teacher("Pak Andi", "Bandung");
+        System.out.println(t.addCourse("Math"));     // true
+        System.out.println(t.addCourse("Math"));     // false
+        System.out.println(t.removeCourse("Math"));  // true
+        System.out.println(t.removeCourse("Math"));  // false
+
+        System.out.println(t);
+    }
+}
